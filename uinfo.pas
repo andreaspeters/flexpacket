66db02ae3ceed2aa5440daac0e83ef93c72e49a0
Andreas Peters <ap@aventer.biz>
2024-11-04 09:58:27 +0100

    ADD: donation link

--- uinfo.pas
+++ uinfo.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls,
-  ButtonPanel;
+  ButtonPanel, LCLIntf;
 
 type
 
@@ -15,11 +15,15 @@ type
   TTFInfo = class(TForm)
     BPDefaultButtons: TButtonPanel;
     Image1: TImage;
+    IGithubDonation: TImage;
     Label1: TLabel;
     Label2: TLabel;
     Label3: TLabel;
+    LGithubDonation: TLabel;
     Memo1: TMemo;
     procedure CloseInfo(Sender: TObject);
+    procedure IGithubDonationClick(Sender: TObject);
+    procedure Memo1Change(Sender: TObject);
   private
 
   public
@@ -38,6 +42,17 @@ implementation
 procedure TTFInfo.CloseInfo(Sender: TObject);
 begin
   Close;
+end;
+
+procedure TTFInfo.IGithubDonationClick(Sender: TObject);
+begin
+  if not OpenURL('https://github.com/sponsors/AVENTER-UG') then
+    ShowMessage('Could not open URL: https://github.com/sponsors/AVENTER-UG');
+end;
+
+procedure TTFInfo.Memo1Change(Sender: TObject);
+begin
+
 end;
 
 end.
