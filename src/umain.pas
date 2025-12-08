99094704e6661e4c943a2e8492fb5f0c4a9b366c
Andreas Peters <ap@aventer.biz>
2025-12-08 19:12:23 +0100

    CHANGE: set label to disconnect after link error.

--- src/umain.pas
+++ src/umain.pas
@@ -1956,7 +1956,7 @@ begin
   if MIEnableAGW.Checked then
     Status := AGWClient.ChannelStatus[Channel];
 
-  if (Status[6] = 'DISCONNECTED') or (Status[5] = Chr(0)) then
+  if (Status[6] = 'DISCONNECTED') or (Status[5] = Chr(0)) or (Status[6] = 'LINK FAILURE') then
   begin
     SetChannelButtonLabel(Channel,'Disc');
     FPConfig.Connected[Channel] := False;
