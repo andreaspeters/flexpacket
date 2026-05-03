05e338a6df967242777eccf3cdeb9dd86075af33
Andreas Peters <andreas@noreply.gitea.aventer.internal>
2026-05-03 12:48:22 +0000

    revert a06a2fa6919e8c3235fb735792c6bcc5f0d7f4df
    revert UPDATE: changelog

--- src/uhostmode.pas
+++ src/uhostmode.pas
@@ -71,6 +71,8 @@ destructor THostMode.Destroy;
 begin
   if Assigned(FSerial) then
   begin
+    SendStringCommand(0, 0, 'JHOST0');
+    sleep(2000);
     FSerial.CloseSocket;
     FreeAndNil(FSerial);
   end;
